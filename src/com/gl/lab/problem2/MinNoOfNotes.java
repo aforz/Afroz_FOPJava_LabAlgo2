8fb0f24755de44fa845b5a7d110fba5ca4776d12
Afroz Alam <afroz3002@gmail.com>
2023-04-02 12:44:26 +0530

    lab2 problems implemtation

--- src/com/gl/lab/problem2/MinNoOfNotes.java
+++ src/com/gl/lab/problem2/MinNoOfNotes.java
@@ -7,24 +7,24 @@ public class MinNoOfNotes {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of currency denominations ");
         int size = sc.nextInt();
-        int[] arr = new int[size];
+        int[] currencyDenominations = new int[size];
         System.out.println("enter the currency denominations value\n");
         for (int i = 0; i < size; i++)
-            arr[i] = sc.nextInt();
+            currencyDenominations[i] = sc.nextInt();
         System.out.println("enter the amount you want to pay");
-        int targetAmt = sc.nextInt();
-        int[] sortedArray = sortArray(arr);
+        int amtWantToPay = sc.nextInt();
+        int[] sortedArray = sortArray(currencyDenominations);
         int[] counts = new int[size];
         for (int i = size - 1; i >= 0; i--) {
-            if (targetAmt >= sortedArray[i]) {
-                counts[i] = targetAmt / sortedArray[i];
-                targetAmt = targetAmt % sortedArray[i];
+            if (amtWantToPay >= sortedArray[i]) {
+                counts[i] = amtWantToPay / sortedArray[i];
+                amtWantToPay = amtWantToPay % sortedArray[i];
             }
         }
         System.out.println("Your payment approach in order to give min no of notes will be");
         for (int i = size - 1; i >= 0; i--) {
             if (counts[i] != 0) {
-                System.out.println(sortedArray[i] + " : " + counts[i]);
+                System.out.println(sortedArray[i] + ":" + counts[i]);
             }
         }
     }
